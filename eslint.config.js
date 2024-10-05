9ddd632bd0129e83f915d6e0a2b9864e93777d17
Atila Fassina <atila@fassina.eu>
2024-10-05 16:09:18 +0200

    chore: init

---
+++ eslint.config.js
@@ -0,0 +1,80 @@
+import eslint from "@eslint/js";
+import comments from "@eslint-community/eslint-plugin-eslint-comments/configs";
+import vitest from "@vitest/eslint-plugin";
+import markdown from "eslint-plugin-markdown";
+import n from "eslint-plugin-n";
+import packageJson from "eslint-plugin-package-json/configs/recommended";
+import * as regexp from "eslint-plugin-regexp";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+	{
+		ignores: [
+			"coverage*",
+			"lib",
+			"node_modules",
+			"pnpm-lock.yaml",
+			"**/*.snap",
+		],
+	},
+	{
+		linterOptions: {
+			reportUnusedDisableDirectives: "error",
+		},
+	},
+	eslint.configs.recommended,
+	...markdown.configs.recommended,
+	comments.recommended,
+	n.configs["flat/recommended"],
+	packageJson,
+	regexp.configs["flat/recommended"],
+	...tseslint.config({
+		extends: tseslint.configs.strictTypeChecked,
+		files: ["**/*.js", "**/*.ts"],
+		languageOptions: {
+			parserOptions: {
+				projectService: {
+					allowDefaultProject: ["*.*s", "eslint.config.js"],
+					defaultProject: "./tsconfig.json",
+				},
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+		rules: {
+			// These on-by-default rules don't work well for this repo and we like them off.
+			"no-constant-condition": "off",
+
+			// These on-by-default rules work well for this repo if configured
+			"@typescript-eslint/no-unused-vars": ["error", { caughtErrors: "all" }],
+		},
+	}),
+	{
+		files: ["*.jsonc"],
+		rules: {
+			"jsonc/comma-dangle": "off",
+			"jsonc/no-comments": "off",
+			"jsonc/sort-keys": "error",
+		},
+	},
+	{
+		extends: [tseslint.configs.disableTypeChecked],
+		files: ["**/*.md/*.ts"],
+		rules: {
+			"n/no-missing-import": ["error", { allowModules: ["shieldwall"] }],
+		},
+	},
+	{
+		files: ["**/*.test.*"],
+		languageOptions: {
+			globals: vitest.environments.env.globals,
+		},
+		plugins: { vitest },
+		rules: {
+			...vitest.configs.recommended.rules,
+
+			// These on-by-default rules aren't useful in test files.
+			"@typescript-eslint/no-unsafe-assignment": "off",
+			"@typescript-eslint/no-unsafe-call": "off",
+		},
+	},
+);
